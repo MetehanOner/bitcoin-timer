fa9293c8943a3b7cbca8031176a7f03822532173
Ömer Özdemir <ge82vik@tum.de>
2022-04-17 15:53:58 +0200

    hitNumber added

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
 let stockPriceElement = document.getElementById('stock-price');
+let hitElement = document.getElementById('hit');
 let lastPrice = null;
 
 ws.onmessage = (event) => {
     let stockObject = JSON.parse(event.data);
+    //console.log(event.data);
     let price = parseFloat(stockObject.p).toFixed(2);
     stockPriceElement.innerText = price;
     stockPriceElement.style.color = !lastPrice || lastPrice === price ? 'black' : price > lastPrice ? 'green' : 'red';
+
+    hitNumber = 40500;
+    if (price == hitNumber | (price > hitNumber & lastPrice < hitNumber & lastPrice) | (price < hitNumber & lastPrice > hitNumber & lastPrice)){
+        var date = new Date(stockObject.T); 
+        hitElement.innerText = "Congrats, hit " + hitNumber + " on " + date.toString();
+    }
+
     lastPrice = price;
 }
